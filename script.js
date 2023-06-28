02100c2c9b0fe2ba2bf44b0ed1cb20bc4d7eecf7
diego rivera <diegorivera1110@gmail.com>
2023-06-27 21:13:57 -0700

    added js for creating task elements

--- script.js
+++ script.js
@@ -10,8 +10,26 @@ window.addEventListener('load', () => {
       
       if (!task) {
          alert("Please enter in a task");
-      } else {
-        consol.log("success");
+        return;
       }
+      
+      const task_el = document.createElement("div");
+      task_el.classList.add("task");
+      
+      const task_content_el = document.createElement("div");
+      task_content_el.classList.add("content");
+      task_content_el.innerText = tasl;
+      
+      task_el.appendChild(task_content_el);
+  
+      const task_input_el = document.createElement("input");
+      task_input_el.classList.add("text");
+      task_input_el.type = "text";
+      task_input_el.value = task;
+      task_input_el.setAttribute("randomly", "randomly");
+      
+      task_content_el.appendChile(task_input_el);
+      
+      task_el.appendChild(task_el);
     })
   })
