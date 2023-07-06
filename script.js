99856c9db3a3a964f4058d7d811f9a2851441cf8
diego rivera <diegorivera1110@gmail.com>
2023-07-05 20:30:25 -0700

    added eventListener for edit

--- script.js
+++ script.js
@@ -48,6 +48,19 @@ window.addEventListener('load', () => {
       
       list_el.appendChild(task_el);
       
-      task_el.appendChild(task_el);
+      input.value = "";
+      
+      task_edit_el.addEventListener("click", () => {
+        if (task_edit_el.innerText.toLowerClass() ==
+           "edit") {
+          task_input_el.removeAttribute("readonly");
+          task_input_el.focus();
+          task_edit_el.innerText = "Save"
+        } else {
+          // console.log("save");
+          task_input_el.setAttribute("readonly", "readonly");
+          task_edit_el.innerText = "Edit";
+        }
+      })
     })
   })
